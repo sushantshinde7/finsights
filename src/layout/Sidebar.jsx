ce1adf06db5e475fde0e72fab4402618d515197a
Sushant Shinde <sushantshinde2024@gmail.com>
2026-08-29 23:58:58 +0530

    feat(sidebar): -logout authentication added in sidebar at last of sidebar pages.

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -3,31 +3,48 @@ import { ROUTES } from "../routes/routes";
 
 import "./Sidebar.css";
 
-import { Home, LayoutDashboard, ArrowLeftRight, BarChart3 } from "lucide-react";
+import {
+  Home,
+  LayoutDashboard,
+  ArrowLeftRight,
+  BarChart3,
+  LogOut,
+} from "lucide-react";
+
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = ({ closeSidebar }) => {
   const navItems = [
-  {
-    path: ROUTES.HOME,
-    label: "Home",
-    icon: <Home size={18} />,
-  },
-  {
-    path: ROUTES.DASHBOARD,
-    label: "Dashboard",
-    icon: <LayoutDashboard size={18} />,
-  },
-  {
-    path: ROUTES.TRANSACTIONS,
-    label: "Transactions",
-    icon: <ArrowLeftRight size={18} />,
-  },
-  {
-    path: ROUTES.INSIGHTS,
-    label: "Insights",
-    icon: <BarChart3 size={18} />,
-  },
-];
+    {
+      path: ROUTES.HOME,
+      label: "Home",
+      icon: <Home size={18} />,
+    },
+    {
+      path: ROUTES.DASHBOARD,
+      label: "Dashboard",
+      icon: <LayoutDashboard size={18} />,
+    },
+    {
+      path: ROUTES.TRANSACTIONS,
+      label: "Transactions",
+      icon: <ArrowLeftRight size={18} />,
+    },
+    {
+      path: ROUTES.INSIGHTS,
+      label: "Insights",
+      icon: <BarChart3 size={18} />,
+    },
+  ];
+
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logout();
+    navigate("/");
+  };
 
   return (
     <div className="sidebar-inner">
@@ -44,6 +61,15 @@ const Sidebar = ({ closeSidebar }) => {
           </NavLink>
         ))}
       </nav>
+      <div className="sidebar-account">
+        <button className="nav-item logout-item" onClick={handleLogout}>
+          <span className="nav-icon">
+            <LogOut size={18} />
+          </span>
+
+          <span>Logout</span>
+        </button>
+      </div>
 
       <div className="sidebar-footer">
         <hr />
